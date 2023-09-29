e712a7a17d32a7903034aa415319a056046f7b4a
karen0830 <lopezkaren43567gmail.com>
2023-09-29 18:49:42 -0500

    Ruta Profile

--- client/src/ProtectedRouter.jsx
+++ client/src/ProtectedRouter.jsx
@@ -5,8 +5,8 @@ import { useAuth } from './context/AuthContext'
 const ProtectedRouter = () => {
     const { loading, user, isAuthenticated } = useAuth()
     console.log(loading, user, isAuthenticated);
-    console.log("Hokakaa");
-    if(!isAuthenticated) return (<Navigate to="/login"/>)
+    if (loading) return <h1>Loading...</h1>
+    if(!loading && !isAuthenticated) return (<Navigate to="/login"/>)
 
     return <Outlet/>
 }
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [errors, setErrors] = useState([])
-    
+
     const signup = async (user) => {
         try {
             console.log(user);
@@ -70,32 +70,39 @@ export const AuthProvider = ({ children }) => {
         }
     }, [errors])
 
+    useEffect(()=> {
+        console.log(user);
+        console.log(isAuthenticated);
+    },[user, isAuthenticated])
+
     useEffect(() => {
         async function checkLogin() {
             const cookies = Cookies.get()
             console.log(cookies);
             if (!cookies.token) {
                 setIsAuthenticated(false)
-                setLoading(false);
+                setLoading(false)
                 return setUser(null);
-            }
-            try {
-                const res = await verityTokenRequest(cookies.token)
-                console.log(res);
-                if (!res.data) {
-                    setIsAuthenticated(false);
+            } else {
+                try {
+                    const res = await verityTokenRequest(cookies.token)
+                    console.log(res.data);
+                    if (!res.data) {
+                        setIsAuthenticated(false);
+                        setLoading(false);
+                        console.log("se metio aca");
+                        return;
+                    } else {
+                        setIsAuthenticated(true)
+                        setUser(res.data)
+                        setLoading(false)
+                    }
+                } catch (error) {
+                    console.log(error);
+                    setIsAuthenticated(false)
+                    setUser(null)
                     setLoading(false);
-                    return ;
                 }
-
-                setIsAuthenticated(true)
-                setUser(res.data)
-                setLoading(false);
-            } catch (error) {
-                console.log(error);
-                setIsAuthenticated(false)
-                setUser(null)
-                setLoading(false);
             }
         }
         checkLogin()
