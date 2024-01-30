e60da62eb141db933a20761d72cdb80bf458581e
karen0830 <lopezkaren43567@gmail.com>
2024-01-30 08:54:26 -0500

    142543

--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ const url = 'mongodb+srv://karen123:karen123@cluster0.yaxtqh3.mongodb.net/test';
 // const url = 'mongodb://localhost:27017/test'
 const connectDB = async () => {
     try {
-        await mongoose.connect(url, {
-            useFindAndModify: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(url,  {
+            // useFindAndModify: false,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
             writeConcern: {
                 w: 'majority',
                 j: true,
