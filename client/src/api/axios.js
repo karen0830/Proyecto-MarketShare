fb02dfb632fd86067951b7fb9ab1fe4eea6a6aaf
karen0830 <lopezkaren43567@gmail.com>
2024-02-07 10:03:38 -0500

    addVideo

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://backend-bql1.onrender.com/api',
     withCredentials: true
 })
 
