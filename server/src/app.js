e18fc4723848979f65c137b6131d51fe322960ad
karen0830 <lopezkaren43567@gmail.com>
2024-02-11 07:20:03 -0500

    cambios proxy inverso

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors({
     origin: 'https://maket-share.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 
