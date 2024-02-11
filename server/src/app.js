640ac16df6d88a5f79238646cec0afcbfe535a07
karen0830 <lopezkaren43567@gmail.com>
2024-02-11 07:01:47 -0500

    cambios proxy inversoa

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import Router from '../src/router/auth.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-import axios from 'axios'
+import * as redbird from 'redbird';
 
 const app = express();
 
@@ -18,13 +18,23 @@ app.use(morgan('dev'));
 app.use(express.json()); // Middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(cookieParser());
 
-
 // Define tus rutas y configura Express
 app.use("/api", Router);
 
 // Redirigir desde la raíz '/' a '/login'
 app.get('/', (req, res) => {
     res.redirect('/api/loginUser'); // Cambia '/api/login' por la ruta correcta de inicio de sesión
+});
+
+// Configuración de Redbird
+let proxy = redbird({port: 80});
+
+proxy.register("maket-share.netlify.app", "https://backend-ve18.onrender.com/api/getAllPublications", {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  }
 });
 
 export default app;
