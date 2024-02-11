e52b73553dfa0c572bf125cc23ecfb7939f9b777
karen0830 <lopezkaren43567@gmail.com>
2024-02-11 10:27:29 -0500

    cambios en headers

--- server/src/middlewares/validateToken.js
+++ server/src/middlewares/validateToken.js
@@ -2,12 +2,13 @@ import jwt from 'jsonwebtoken'
 import { TOKEN_SECRET } from '../config.js';
 
 export const authRequired = (req, res, next) => {
-    const {token} = req.cookies;
-    console.log(token);
-
-    if(!token) return res.status(401).json({
-        message: "No token, suthorization denied"
-    })
+    const authorizationHeader = req.headers['authorization'];
+    const token = authorizationHeader.split(' ')[1]; // Obtén solo el token, omitiendo 'Bearer'
+    // const token = req.cookies.token;
+    console.log("Tokencito " , token);
+    if (token === 'null') {
+        return res.status(401).json({message: "No token, authorization denied"});
+    }
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if(err) return  res.status(403).json({message: "Invalid token"})
