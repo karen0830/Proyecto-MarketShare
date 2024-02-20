fa459d7f74963e20c36e5fc880fea9241efcab80
karen0830 <lopezkaren43567@gmail.com>
2024-02-20 10:01:04 -0500

    Merge

--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,9 @@ import routerShared from './router/Shared.routes.js';
 
 const app = express();
 // http://localhost:5173
-// https://maket-share.netlify.app
+// https://main--maket-share.netlify.app
 app.use(cors({
-    origin: 'https://main--maket-share.netlify.app',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
