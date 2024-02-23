24b1f81893d908f3e029cf0e72b7b6361c06560a
karen0830 <lopezkaren43567@gmail.com>
2024-02-23 06:58:54 -0500

    Rama

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // http://localhost:5173
 // https://main--maket-share.netlify.app
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://main--maket-share.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
