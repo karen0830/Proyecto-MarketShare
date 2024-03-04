01b6f9b2af8d7bcbba521fb8d76c940af7fd9418
karen0830 <lopezkaren43567@gmail.com>
2024-03-03 19:44:11 -0500

    DM

--- server/src/app.js
+++ server/src/app.js
@@ -10,8 +10,10 @@ import routerProduct from './router/products.auth.routes.js';
 const app = express();
 // http://localhost:5173
 // 'https://main--marketshare.netlify.app
+ // origin: ['https://main--marketshare-ecommerce.netlify.app/', "https://main--marketshare.netlify.app"],
 app.use(cors({
-    origin: ['https://65e4ee53efa88c0008c9136f--ecommerce-marketshare.netlify.app', "https://main--marketshare.netlify.app"],
+    // origin: ['http://localhost:5173', "http://localhost:5174"],
+    origin: ['https://main--marketshare-ecommerce.netlify.app/', "https://main--marketshare.netlify.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
