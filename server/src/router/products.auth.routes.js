cda9cf8bc3210d1ae95fa0f9502b87c1aee622b1
JEBOIDOR <115515518+JEBOIDOR@users.noreply.github.com>
2024-03-04 22:00:01 -0500

    Delete server directory

--- server/src/router/products.auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authRequired } from "../middlewares/validateToken.js";
-import { getAllProductCompany } from "../controllers/products.auth.controller.js";
-const routerProduct = Router();
-
-routerProduct.get("/getAllProductsCompany", getAllProductCompany)
-
-export default routerProduct;
